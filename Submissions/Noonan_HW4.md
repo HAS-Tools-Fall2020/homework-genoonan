5f9ced41b96c8705484b96039186fa003a878a0b
Gillian Noonan <gillianerin@gmail.com>
2020-09-19 21:08:18 -0700

    think i'm starting to get this!

--- Submissions/Noonan_HW4.md
+++ Submissions/Noonan_HW4.md
@@ -17,21 +17,23 @@
 
 
   - *What type of values is it composed of?*  
-      - float64
+      - float64.  Column 0 is year, column 1 is month, column 2 is day, column 3 is flow rate.
 
 
-  - *What is are its dimensions, and total size?*
+  - *What are its dimensions, and total size?*
     - Flow_data is two-dimensional (2) array and shape is (11584, 4) which means 11584 rows and 4 columns.
 
 
 **3) How many times was the daily flow greater than your prediction in the month of September (express your answer in terms of the total number of times and as a percentage)?**
 
-- Answer: ...
+- Answer: Flow was greater than my prediction 915 times.  There are 948 total values recorded for September so the percentage of times greater is 96.5%!
 
 
 **4) How would your answer to the previous question change if you considered only daily flows in or before 2000? Same question for the flows in or after the year 2010? (again report total number of times and percentage)**
 
-- Answer: ...
+- Answer:
+  - In or before 2000: Flow was greater than my prediction 360 times.  Total September data values in or before 2000 is 360, so 100%!
+  - In or after 2010: Flow was greater than my prediction 299 times.  Total September data values in or before 2000 is 318, so 94%!
 
 
 **5) How does the daily flow generally change from the first half of September to the second?**
--- assignment_4/Noonan_HW4.py
+++ assignment_4/Noonan_HW4.py
@@ -20,21 +20,17 @@ print(filepath)
 # should use for the rest of the assignment
 # no need to worry about how this is being done now we will cover
 # this in later sections. 
-
 #Read the data into a pandas dataframe
 data=pd.read_table(filepath, sep = '\t', skiprows=30,
         names=['agency_cd', 'site_no', 'datetime', 'flow', 'code']
         )
-
 # Expand the dates to year month day
 data[["year", "month", "day"]] =data["datetime"].str.split("-", expand=True)
 data['year'] = data['year'].astype(int)
 data['month'] = data['month'].astype(int)
 data['day'] = data['day'].astype(int)
-
 # Make a numpy array of this data
 flow_data = data[['year', 'month','day', 'flow']].to_numpy()
-
 # Getting rid of the pandas dataframe since we wont be using it this week
 del(data)
 
@@ -43,6 +39,7 @@ del(data)
 # Jill Question Answering Code
 #%%
 print(flow_data)
+
 # %%
 type(flow_data)
 
@@ -51,6 +48,7 @@ flow_data.dtype
 
 # %%
 flow_data.ndim
+
 # %%
 flow_data.shape
 
@@ -74,31 +72,55 @@ print((flow_count_greater / flow_count_Sep)*100)
 # Count the number of values with flow > Wk4prediction and month ==9 and year <=2000
 flow_count_greater_1989_2000 = np.sum((flow_data[:,3] > Wk4_prediction) & (flow_data[:,1]==9) & (flow_data[:,0]<=2000))
 print(flow_count_greater_1989_2000)
-
-#%%
 # Count the number of total values with month ==9 and year <=2000
 flow_count_Sep_1989_2000 = np.sum((flow_data[:,1]==9) & (flow_data[:,0]<=2000))
 print(flow_count_Sep_1989_2000) 
-
 #calculate percentage
 print((flow_count_greater_1989_2000 / flow_count_Sep_1989_2000)*100)
 
 #%%
-#In or before 2000: 
-# Count the number of values with flow > Wk4prediction and month ==9 and year <=2000
-flow_count_greater_1989_2000 = np.sum((flow_data[:,3] > Wk4_prediction) & (flow_data[:,1]==9) & (flow_data[:,0]<=2000))
-print(flow_count_greater_1989_2000)
-
-#%%
-# Count the number of total values with month ==9 and year <=2000
-flow_count_Sep_1989_2000 = np.sum((flow_data[:,1]==9) & (flow_data[:,0]<=2000))
-print(flow_count_Sep_1989_2000) 
-
+#In or after 2010: 
+# Count the number of values with flow > Wk4prediction and month ==9 and year >=2010
+flow_count_greater_2010_2020 = np.sum((flow_data[:,3] > Wk4_prediction) & (flow_data[:,1]==9) & (flow_data[:,0]>=2010))
+print(flow_count_greater_2010_2020)
+# Count the number of total values with month ==9 and year >=2010
+flow_count_Sep_2010_2020 = np.sum((flow_data[:,1]==9) & (flow_data[:,0]>=2010))
+print(flow_count_Sep_2010_2020) 
 #calculate percentage
-print((flow_count_greater_1989_2000 / flow_count_Sep_1989_2000)*100)
-#%%
-# Calculate the average flow for these same criteria 
-flow_mean = np.mean(flow_data[(flow_data[:,3] > 600) & (flow_data[:,1]==7),3])
+print((flow_count_greater_2010_2020 / flow_count_Sep_2010_2020)*100)
 
-print("Flow meets this critera", flow_count, " times")
-print('And has an average value of', flow_mean, "when this is true")
+
+#%%
+# Calculate the average flow for 1st half of September
+print(flow_data)
+flow_mean_Sep_begin = np.mean(flow_data[(flow_data[:,1]==9) & (flow_data[:,2]<=15), 3])
+print(flow_mean_Sep_begin)
+
+#%%
+# Calculate the average flow for 2nd half of September
+flow_mean_Sep_begin = np.mean(flow_data[(flow_data[:,1]==9) & (flow_data[:,2]>15), 3])
+print(flow_mean_Sep_begin)
+
+# %%
+# Make a histogram of data
+# Use the linspace  funciton to create a set  of evenly spaced bins
+mybins = np.linspace(0, 1000, num=15)
+# another example using the max flow to set the upper limit for the bins
+#mybins = np.linspace(0, np.max(flow_data[:,3]), num=15) 
+#Plotting the histogram
+plt.hist(flow_data[:,3], bins = mybins)
+plt.title('Streamflow')
+plt.xlabel('Flow [cfs]')
+plt.ylabel('Count')
+
+
+#%%
+# Attempt: Trying to isolate just September
+flow_data_Sep = flow_data[flow_data[:,1] ==9]
+mybins = np.linspace(0, 1000, num=20)
+plt.hist(flow_data_Sep[:,3], bins = mybins)
+plt.title('September Streamflow')
+plt.xlabel('Flow [cfs]')
+plt.ylabel('Count')
+
+# %%
