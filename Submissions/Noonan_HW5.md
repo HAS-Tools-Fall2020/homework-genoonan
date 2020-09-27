bc9acc19a3bd6f127544b445244e96825140b142
genoonan <gillianerin@gmail.com>
2020-09-27 13:38:39 -0700

    sunday work -take 2

--- Submissions/Noonan_HW5.md
+++ Submissions/Noonan_HW5.md
@@ -32,10 +32,10 @@ _Figure 4 - flow statistics_monthly_
 **4. Provide a table with the 5 highest and 5 lowest flow values for  the period of record. Include the date, month and flow values in your summary**  
 
 _Figure 4 - Five highest flow values_  
-![](assets/Noonan_HW5-dfe8877c.JPG)
+![](assets/Noonan_HW5-a8d995ab.JPG)
 
 _Figure 5 - Five lowest flow values_  
-![](assets/Noonan_HW5-433db8f5.JPG)
+![](assets/Noonan_HW5-9ee83c5e.JPG)
 
 **5.  Find the highest and lowest flow  values for every month of the year (i.e. you will find 12 maxes and 12 mins) and report back what year these occurred in.**  
 
@@ -50,20 +50,20 @@ _Figure 8 - Min Flow by Month_
 
 
 
-  |Month|Year|Min Flow|Max Flow|
-  |-----|-----|-----|-----|
-  |Jan||158|63400|
-  |Feb||136|61000|
-  |Mar||97|30500|
-  |Apr||64.9|4690|
-  |May||46|546|
-  |June||22.1|481|
-  |July||19|1040|
-  |Aug||29.6|5360|
-  |Sep||36.6|5590|
-  |Oct||69.9|1910|
-  |Nov||117|4600|
-  |Dec||155|28700|
+  |Month|Year-Min|Min Flow|Year Max|Max Flow|
+  |-----|-----|-----|-----|-----|
+  |Jan|2003|158|1993|63400|
+  |Feb|1991|136|1993|61000|
+  |Mar|1989|97|1995|30500|
+  |Apr|2018|64.9|1991|4690|
+  |May|2004|46|1992|546|
+  |June|2012|22.1|1992|481|
+  |July|2012|19|2006|1040|
+  |Aug|2019|29.6|1992|5360|
+  |Sep|2020|36.6|2004|5590|
+  |Oct|2012|69.9|2010|1910|
+  |Nov|2016|117|2004|4600|
+  |Dec|2012|155|2004|28700|
 
 **6. Provide a list of historical dates with flows that are within 10% of your week 1 forecast value. If there are none than increase the %10 window until you have at least one other  value and report the date and the new window you used**
 
--- assignment_5/Noonan_HW5_draft.py
+++ assignment_5/Noonan_HW5_draft.py
@@ -38,30 +38,6 @@ data.head(3)
 data.shape
 
 # %%
-# TESTING 
-# PANDAS .plot() method
-f, ax = plt.subplots()
-data.plot(x="month", y="flow", title="flow vs. month", ax=ax)
-plt.show()
-
-# %%
-# TESTING 
-# MATPLOTLIB TO DO SAME (more flexible approach) - BUT HOW TO GET X AND Y LABELS?
-f, ax = plt.subplots()
-ax.plot(data.month,
-        data.flow)
-ax.set(title="flow vs. month")
-plt.show()
-
-# %%
-# TESTING 
-# MATPLOTLIB ax.bar() method
-f, ax = plt.subplots()
-ax.bar(x=data.month,
-       height=data.flow)
-plt.show()
-
-# %%
 #1) Provide a summary of the data frames properties.
 data.shape
 
@@ -87,30 +63,88 @@ data.groupby("month")["flow"].describe()
 
 #%%
 #4)Provide a table with the 5 highest and 5 lowest flow values for the period of record. Include the date, month and flow values in your summary
-# HOW TO GET RID OF THE UNWANTED COLUMNS OR PULL OUT ONLY DESIRED COLUMNS FROM TABLE?
-data2 = data.sort_values(by="flow", ascending=False)
+data2 = data[['datetime', 'month', 'flow']]
+data2
+
+#%%
+data2.sort_values(by="flow", ascending=False)
+#%%
 data2.head(5)
 #%%
 data2.tail(5)
 
 #%%
 #5)Find the highest and lowest flow values for every month of the year (i.e. you will find 12 maxes and 12 mins) and report back what year these occurred in
-# THERE HAS GOT TO BE A MORE EFFICIENT WAY TO DO THIS.....
-# HOW TO GET YEAR??
+# THIS WORKS BUT IT'S CLUNKY AND LOOOOONG.  THERE HAS GOT TO BE A MORE EFFICIENT WAY TO DO THIS I'M SURE!!.....for now, it gives me the answers need though......
 flow_by_month=data.groupby(["month"])[["flow"]].describe()
-flow_by_month
-
-#%%
-# # max_flow_by_month=data.groupby(["month"])[["flow"]].max()
-# max_flow_by_month
-#%%
-# min_flow_by_month=data.groupby(["month"])[["flow"]].min()
-# min_flow_by_month
-
-#%%
 flow_by_month.columns=flow_by_month.columns.droplevel(0)
 flow_by_month
 
+#%%
+data3= data[["year", "month", "flow"]]
+data3
+
+#PAINSTAKINGLY PULLING OUT YEARS 
+#%%
+x = 1
+data_Jan = data3[data3["month"] == x]
+print(data_Jan.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Jan.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 2
+data_Feb = data3[data3["month"] == x]
+print(data_Feb.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Feb.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 3
+data_Mar = data3[data3["month"] == x]
+print(data_Mar.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Mar.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 4
+data_Apr = data3[data3["month"] == x]
+print(data_Apr.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Apr.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 5
+data_May = data3[data3["month"] == x]
+print(data_May.sort_values(by = "flow", ascending = False).tail(1))
+print(data_May.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 6
+data_Jun = data3[data3["month"] == x]
+print(data_Jun.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Jun.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 7
+data_Jul = data3[data3["month"] == x]
+print(data_Jul.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Jul.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 8
+data_Aug = data3[data3["month"] == x]
+print(data_Aug.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Aug.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 9
+data_Sep = data3[data3["month"] == x]
+print(data_Sep.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Sep.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 10
+data_Oct = data3[data3["month"] == x]
+print(data_Oct.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Oct.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 11
+data_Nov = data3[data3["month"] == x]
+print(data_Nov.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Nov.sort_values(by = "flow", ascending = False).head(1))
+#%%
+x = 12
+data_Dec = data3[data3["month"] == x]
+print(data_Dec.sort_values(by = "flow", ascending = False).tail(1))
+print(data_Dec.sort_values(by = "flow", ascending = False).head(1))
 #%%
 # Plot Max Monthly Flow
 f, ax = plt.subplots()
@@ -132,12 +166,6 @@ ax.bar(flow_by_month.index,
 
 ax.set(title="Min Monthly Flow")
 plt.show()
-
-#%%
-x = 158
-data[data["flow"] == x]
-
-
 
 #%%
 #6)Provide a list of historical dates with flows that are within 10% of your week 1 forecast value. If there are none than increase the %10 window until you have at least one other value and report the date and the new window you used
